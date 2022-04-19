537a5864824849a34a3e568d2569f1d4cd63aed8
ChangMin Lee <cmlee@wkit.co.kr>
2022-04-19 17:55:34 +0900

    [feat] DI 응용

--- src/main/java/com/toby/springoftoby/SpringOfTobyApplication.java
+++ src/main/java/com/toby/springoftoby/SpringOfTobyApplication.java
@@ -1,17 +1,24 @@
 package com.toby.springoftoby;
 
+import com.toby.springoftoby.user.dao.CountingConnectionMaker;
+import com.toby.springoftoby.user.dao.CountingDaoFactory;
+import com.toby.springoftoby.user.dao.DaoFactory;
 import com.toby.springoftoby.user.dao.UserDao;
 import com.toby.springoftoby.user.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class SpringOfTobyApplication {
 
-//    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        UserDao dao = new UserDao();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//
+//        UserDao dao = context.getBean("userDao", UserDao.class);
 //
 //        User user = new User();
 //        user.setId("whiteship");
@@ -26,6 +33,13 @@ public class SpringOfTobyApplication {
 //        System.out.println(user2.getName());
 //        System.out.println(user2.getPassword());
 //        System.out.println(user2.getId()+"조회 성공");
-//    }
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+
+        UserDao dao = context.getBean("userDao",UserDao.class);
+
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection counter : "+ccm.getCounter());
+    }
 
 }
--- src/main/java/com/toby/springoftoby/user/dao/CountingDaoFactory.java
+++ src/main/java/com/toby/springoftoby/user/dao/CountingDaoFactory.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DaoFactory {
-
+public class CountingDaoFactory {
     @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
     @Bean
-    public ConnectionMaker connectionMaker(){
+    public ConnectionMaker connectionMaker() {
+        return new CountingConnectionMaker(realConnectionMaker());
+    }
+
+    @Bean
+    public ConnectionMaker realConnectionMaker() {
         return new DConnectionMaker();
     }
 }
